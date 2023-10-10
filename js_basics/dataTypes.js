526b71a1bd58a08706663b99a935f090e9ceab82
Sumit Kumar <138772172+dazlingJava@users.noreply.github.com>
2023-10-10 10:49:50 +0000

    Data type & variables

---
+++ js_basics/dataTypes.js
@@ -0,0 +1,30 @@
+//"use strict";  // treat all JS code as newer version
+
+// alert (3 + 3) we are using nodejs, not a browser
+
+console.log(4+4);           // code readibilty should be high
+
+console.log("Sumit")    // for documentation follow two site mdn or tc.39es (ECMA)
+
+
+// dataTypes :-  E.g.
+
+let name = "Sumit Kumar"
+let age = 35
+let isLoggedIn = true
+
+/* 
+number  2 to power 53 take int value
+bigint   take big int value 
+string  => "Sumit" take string (text Value)
+boolean  => take true or flase
+null => standalone value (that means Empty)  
+undefined  => currently value undefined      undefined is type
+symbol  => unique
+
+object
+*/
+
+console.log(typeof "sumit kumar");  // it tell us which data type is defined) 
+console.log(typeof null);            //null is object 
+console.log(typeof undefined);        //undefined is type
---
+++ js_basics/variable.js
@@ -0,0 +1,22 @@
+const accountId = 10188
+let accountEmail = "sum@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountState;
+
+
+// single line comments
+/* prefer not to use var becauseof issue 
+in block scope and functiona scope */
+
+// accountId = 45 not allowed
+
+
+console.log(accountId);
+
+accountEmail = "sun@gmail.com"
+accountPassword = "24567"
+accountCity = "New Delhi"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
